68098e4580af3a269a0935043c07cd506a78d836
Karthik Vishal <karthikvishal1506@gmail.com>
2026-02-13 10:12:35 +0530

    Header changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,6 +47,7 @@ export default function Header() {
             />
             <button className="absolute right-2 top-1/2 -translate-y-1/2">
               <Search className="w-5 h-5" />
+
             </button>
           </div>
         </div>
